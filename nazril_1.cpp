077f9301a1e718d490bb2c8fb9fdb500d4d4e225
Nazrililyas123 <111737041+Nazrililyas123@users.noreply.github.com>
2022-09-19 05:17:50 -0700

    Add files via upload

---
+++ nazril_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout <<"Nama:guntur"<<endl;
+
+    cout <<"kelas: X rpl 2"<<endl;
+    
+    cout <<"nilai: 85"<<endl;
+    return 0;
+}
---
+++ nazril_2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	string nama;
+	cout << "Tulisan nama:";
+	cin >> nama;
+	
+	cout << "hi" << nama;
+	cout << "welcome to club!" << endl;
+	
+	return 0;
+}
